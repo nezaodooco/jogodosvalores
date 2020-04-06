2fb0271e1a035005849713e67ff1c01f908831ff
nezaodooco <bruce_gabriel_rossini@hotmail.com>
2020-04-06 11:17:34 -0300

    Projeto criado

---
+++ src/main/java/com/bruce/jogodosvalores/JogodosvaloresApplication.java
@@ -0,0 +1,13 @@
+package com.bruce.jogodosvalores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogodosvaloresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogodosvaloresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruce/jogodosvalores/JogodosvaloresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruce.jogodosvalores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogodosvaloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
